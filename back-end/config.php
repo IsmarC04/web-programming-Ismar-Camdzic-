b69e35e3078326c7d8f5c71875fda048fa272e2c
IsmarC04 <ismar.camdzic@stu.ibu.edu.ba>
2025-11-02 23:27:46 +0100

    Add files via upload

---
+++ back-end/config.php
@@ -0,0 +1,24 @@
+<?php
+
+// Set the reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL ^ (E_NOTICE | E_DEPRECATED));
+
+class Config {
+    public static function DB_NAME() {
+        return  "frizzfactory";
+    }
+    public static function DB_PORT() {
+        return 3306;
+    }
+    public static function DB_USER() {
+        return 'root';
+    }
+    public static function DB_PASSWORD() {
+        return 'Root';
+    }
+    public static function DB_HOST() {
+        return 'localhost';
+    }
+}
