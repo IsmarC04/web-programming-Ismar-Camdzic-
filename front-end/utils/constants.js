2d6600e3521105fa9f2040040808bef94120d81a
IsmarC04 <ismar.camdzic@stu.ibu.edu.ba>
2025-12-28 22:12:03 +0100

    update path

--- front-end/utils/constants.js
+++ front-end/utils/constants.js
@@ -1,8 +1,16 @@
+const PRODUCTION_BASE =
+  "https://web-programming-ismar-camdzic.onrender.com/back-end/";
+
 let Constants = {
   PROJECT_BASE_URL:
     location.hostname == "localhost"
       ? "http://localhost/web-programming-Ismar-Camdzic-/back-end/"
-      : "https://web-programming-ismar-camdzic.onrender.com/",
+      : PRODUCTION_BASE,
   USER_ROLE: "user",
   ADMIN_ROLE: "admin",
+  getApiUrl(path = "") {
+    // ensure exactly one trailing slash on base and no leading slash on path
+    const base = String(this.PROJECT_BASE_URL).replace(/\/+$/, "") + "/";
+    return base + String(path).replace(/^\/+/, "");
+  },
 };
