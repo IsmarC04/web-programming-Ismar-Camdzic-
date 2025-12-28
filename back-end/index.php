54c3f8ea03a7c24a35bbb1dd695872048c72cfa3
IsmarC04 <ismar.camdzic@stu.ibu.edu.ba>
2025-12-28 20:51:28 +0100

    update backend

--- back-end/index.php
+++ back-end/index.php
@@ -2,6 +2,16 @@
 require 'vendor/autoload.php';
 
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 
 require_once __DIR__ . '/services/userService.php';
 require_once __DIR__ . '/services/adminService.php';
