02b2b4400ec51fb313a6931a9325704737d39a57
IsmarC04 <ismar.camdzic@stu.ibu.edu.ba>
2025-12-28 21:05:06 +0100

    update backend

--- back-end/index.php
+++ back-end/index.php
@@ -2,11 +2,12 @@
 require 'vendor/autoload.php';
 
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '*';
+header("Access-Control-Allow-Origin: $origin");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-
+// OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
