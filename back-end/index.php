3e81ad98ddb439f0d1739a4130b51023cb4ad759
IsmarC04 <ismar.camdzic@stu.ibu.edu.ba>
2025-12-28 21:48:58 +0100

    UPDATE BACKEND

--- back-end/index.php
+++ back-end/index.php
@@ -2,16 +2,20 @@
 require 'vendor/autoload.php';
 
 
-$origin = $_SERVER['HTTP_ORIGIN'] ?? '*';
-header("Access-Control-Allow-Origin: $origin");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+// CORS middleware za sve rute
+Flight::before('start', function(&$params, &$output){
+    $origin = $_SERVER['HTTP_ORIGIN'] ?? '*';
+    header("Access-Control-Allow-Origin: $origin");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-// OPTIONS request
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
+    // Ako je OPTIONS request (preflight), odmah završi
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        http_response_code(200);
+        exit();
+    }
+});
+
 
 
 require_once __DIR__ . '/services/userService.php';
